4b18811ed813aa134beff23b32bb8d603a6c164f
Papr1ka <55957109+Papr1ka@users.noreply.github.com>
2022-08-08 20:48:53 +0300

    Update settings.py

--- social/social/settings.py
+++ social/social/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*9k@edpxx4kj3&r!=1r((i5vuxe#u$t90^5qcb*07lknx+vt@*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'connection-net.herokuapp.com']
 
